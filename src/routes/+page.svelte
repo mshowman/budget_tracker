94e2e65fb8c929184a2edd886e23abd141f55f7e
MD Showman <mdshowman@gmail.com>
2023-03-24 17:18:25 -0400

    adds modal instead of navigating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,61 @@
 <script lang="ts">
-	import { Category } from '$lib/models/Item';
+	import { Category, type Item } from '$lib/models/Item';
 	import { database, items } from '$lib/stores/DatabaseStore';
-	import { Button, Card, Container, Grid, Group, Text, Title, Affix } from '@svelteuidev/core';
+	import {
+		Button,
+		Card,
+		Container,
+		Grid,
+		Group,
+		Text,
+		Title,
+		Affix,
+		Modal
+	} from '@svelteuidev/core';
 	import { format, parseISO } from 'date-fns';
+	import { TriangleRight } from 'radix-icons-svelte';
+
+	let modalOpen = false;
+	let itemForModal: Item | undefined;
 
 	async function addItem() {
 		await database.addItem('Test', Math.random() * 1000);
 	}
 
 	function viewItem(id: string) {
-		location.href = `/items/${id}`;
+		itemForModal = $items.find((item) => item.id === id);
+		modalOpen = true;
+		// location.href = `/items/${id}`;
 	}
 
 	async function removeItem(id: string) {
 		await database.removeItem(id);
 	}
+
+	function closeModal() {
+		itemForModal = undefined;
+		modalOpen = false;
+	}
 </script>
 
 <slot>
+	<Modal bind:opened={modalOpen} centered on:close={closeModal}>
+		<Container mt={12} size="xl">
+			<Title>Item {itemForModal?.name}</Title>
+			<Text my={15}>There can be whatever here. Can edit info if wanted.</Text>
+			<Text my={15}>
+				<TriangleRight />
+				{itemForModal?.amount.toLocaleString('en-US', {
+					style: 'currency',
+					currency: 'USD'
+				})}
+			</Text>
+			<Text my={15}>
+				<TriangleRight />
+				{format(parseISO(itemForModal?.date ?? ''), 'Pp')}
+			</Text>
+		</Container>
+	</Modal>
 	<Grid cols={14} justify="center" align="flex-start" mt={12}>
 		<Grid.Col span={1}>
 			<Container size="sm">
@@ -39,22 +77,13 @@
 				>
 					<Card.Section m="sm" p="sm">
 						<Title weight="bold" align="center" order={4}>
-							{item.category ?? 'UNSET'}
+							{item.name}
 						</Title>
 						<Title weight="semibold" align="center" order={5} mt={4}>
-							{format(parseISO(item.date), 'Pp')}
+							{format(parseISO(item.date), 'P')}
 						</Title>
+						<Text weight="semibold" align="center">{item.category ?? 'UNSET'}</Text>
 					</Card.Section>
-
-					<Group position="apart">
-						<Text weight="semibold">{item.name}</Text>
-						<Text>
-							{item.amount.toLocaleString('en-US', {
-								style: 'currency',
-								currency: 'USD'
-							})}
-						</Text>
-					</Group>
 
 					<Group position="center" mt={8} mb={0}>
 						<Button
